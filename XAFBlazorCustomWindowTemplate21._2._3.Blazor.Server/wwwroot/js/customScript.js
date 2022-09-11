5de4bf5c2f2371bd8e5187a87760daa11cd42fbf
xaridev <xaridev@gmail.com>
2022-09-11 15:57:41 -0400

    add floating menu

--- XAFBlazorCustomWindowTemplate21._2._3.Blazor.Server/wwwroot/js/customScript.js
+++ XAFBlazorCustomWindowTemplate21._2._3.Blazor.Server/wwwroot/js/customScript.js
@@ -6,3 +6,30 @@ globalThis.customScript = {
         globalThis.customScript.showDetailViewController.invokeMethodAsync("ShowDetailView", objectId);
     },
 }
+
+function OpenMenu() {
+    var menu = document.querySelector('#walkme-menu');
+    var button = document.querySelector('#walkme-player');
+
+    if (button != null) {
+        
+        button.style.opacity = 0;
+       // button.style.transition = 'display 40000ms ease 0ms !important;';
+        
+        menu.style.opacity = 1;
+       // menu.style.transition = 'display 40000ms ease 0ms !important;';
+    }
+        
+}
+function CloseMenu() {
+    var menu = document.querySelector('#walkme-menu');
+    var button = document.querySelector('#walkme-player');
+
+    if (menu != null) {
+        menu.style.opacity = 0;
+        //button.classList.add("is-show");
+        button.style.opacity = 1;
+       
+    }
+
+}
